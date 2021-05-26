41615aa96a666bdd1b35ccdc80b933a7459c4d4a
Reinaldo <reinaldo800@gmail.com>
2021-05-26 15:46:14 -0300

    test: ensure SignUpController return an error if no name is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -13,5 +13,6 @@ describe('SignUp Controller', () => {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     // to equal compara os valores dos objetos
+    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
   })
 })
